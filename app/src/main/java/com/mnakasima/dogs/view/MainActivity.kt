18a52fbaacf86bbab704aea869666bceef441b42
Maruko <marco.k95@hotmail.com>
2020-01-09 18:27:21 -0300

    changed app icon

--- app/src/main/java/com/mnakasima/dogs/view/MainActivity.kt
+++ app/src/main/java/com/mnakasima/dogs/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mnakasima.dogs
+package com.mnakasima.dogs.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mnakasima.dogs.R
 
 class MainActivity : AppCompatActivity() {
 
