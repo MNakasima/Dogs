d4a0147cf142da13a2179c09bb390457d3050e4a
Maruko <marco.k95@hotmail.com>
2020-01-15 16:33:09 -0300

    getting remote data

--- app/src/main/java/com/mnakasima/dogs/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/mnakasima/dogs/viewmodel/ListViewModel.kt
@@ -3,8 +3,16 @@ package com.mnakasima.dogs.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mnakasima.dogs.model.DogBreed
+import com.mnakasima.dogs.model.DogsApiService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class ListViewModel:ViewModel(){
+
+    private val dogsService = DogsApiService()
+    private val disposable = CompositeDisposable()
 
     val dogs = MutableLiveData<List<DogBreed>>()
     val dogsLoadError = MutableLiveData<Boolean>()
@@ -12,16 +20,40 @@ class ListViewModel:ViewModel(){
 
     fun refresh(){
 
-        val dog1 = DogBreed("1", "Corgi", "15 years", "breedGroup", "breedFor", "temperament", "")
-        val dog2 = DogBreed("2", "Labrador", "10 years", "breedGroup", "breedFor", "temperament", "")
-        val dog3 = DogBreed("3", "Rotwailer", "20 years", "breedGroup", "breedFor", "temperament", "")
+        fetchFromRemote()
 
-        val dogList = arrayListOf<DogBreed>(dog1,dog2,dog3)
+    }
 
-        dogs.value = dogList
-        dogsLoadError.value = false
-        loading.value = false
+    private fun fetchFromRemote(){
 
+        loading.value = true
+
+        disposable.add(
+            dogsService.getDogs()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object: DisposableSingleObserver<List<DogBreed>>(){
+
+                    override fun onSuccess(dogList: List<DogBreed>) {
+                        dogs.value =  dogList
+                        dogsLoadError.value = false
+                        loading.value = true
+                    }
+
+                    override fun onError(e: Throwable) {
+                        dogsLoadError.value = true
+                        loading.value = false
+                        e.printStackTrace()
+                    }
+
+                })
+        )
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
     }
 
 }
