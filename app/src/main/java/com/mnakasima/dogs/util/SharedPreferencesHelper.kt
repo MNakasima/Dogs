7a2e8ae2413297cc8f4728d68326c2902321221d
Maruko <marco.k95@hotmail.com>
2020-01-17 16:51:35 -0300

    retrieving data from database

--- app/src/main/java/com/mnakasima/dogs/util/SharedPreferencesHelper.kt
+++ app/src/main/java/com/mnakasima/dogs/util/SharedPreferencesHelper.kt
@@ -33,4 +33,7 @@ class SharedPreferencesHelper {
             putLong(PREF_TIME, time)
         }
     }
+
+    fun getUpdateTime() = prefs?.getLong(PREF_TIME, 0)
+
 }
--- app/src/main/java/com/mnakasima/dogs/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/mnakasima/dogs/viewmodel/ListViewModel.kt
@@ -1,6 +1,7 @@
 package com.mnakasima.dogs.viewmodel
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mnakasima.dogs.model.DogBreed
@@ -16,6 +17,7 @@ import kotlinx.coroutines.launch
 class ListViewModel(application: Application):BaseViewModel(application){
 
     private var prefHelper = SharedPreferencesHelper(getApplication())
+    private var refreshTime = 5 * 60 * 1000 * 1000 * 1000L
 
     private val dogsService = DogsApiService()
     private val disposable = CompositeDisposable()
@@ -26,8 +28,25 @@ class ListViewModel(application: Application):BaseViewModel(application){
 
     fun refresh(){
 
-        fetchFromRemote()
+        val updateTime = prefHelper.getUpdateTime()
+        if(updateTime != null && updateTime != 0L && System.nanoTime() - updateTime < refreshTime){
+            fetchFromDatabase()
+        }else{
+            fetchFromRemote()
+        }
+    }
 
+    fun refreshBypassCache(){
+        fetchFromRemote()
+    }
+
+    private fun fetchFromDatabase(){
+        loading.value = true
+        launch{
+            val dogs = DogDatabase(getApplication()).DogDao().getAllDogs()
+            dogsRetrieved(dogs)
+            Toast.makeText(getApplication(), "Dogs Retrieved from Database", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun fetchFromRemote(){
@@ -42,6 +61,7 @@ class ListViewModel(application: Application):BaseViewModel(application){
 
                     override fun onSuccess(dogList: List<DogBreed>) {
                         storeDogsLocally(dogList)
+                        Toast.makeText(getApplication(), "Dogs Retrieved from Endpoint", Toast.LENGTH_SHORT).show()
                     }
 
                     override fun onError(e: Throwable) {
