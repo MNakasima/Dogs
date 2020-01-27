5b87d178c02b6658cd8844ebe3a6474129144be4
Maruko <marco.k95@hotmail.com>
2020-01-27 16:32:26 -0300

    building menu at detail screen

--- app/src/main/java/com/mnakasima/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/mnakasima/dogs/view/DetailFragment.kt
@@ -4,10 +4,8 @@ package com.mnakasima.dogs.view
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -40,6 +38,7 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
         return dataBinding.root
     }
@@ -91,6 +90,27 @@ class DetailFragment : Fragment() {
                 }
 
             })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.detail_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.action_send_sms -> {
+
+            }
+
+            R.id.action_share -> {
+
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
 }
